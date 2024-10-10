8a42c1d79ff014cccadfd1f235299557f4e146a4
trgiahuy1710 <23010069@st.phenikaa-uni.edu.vn>
2024-10-10 16:13:41 +0700

    frist commit

---
+++ src/model/subProduct2.java
@@ -0,0 +1,9 @@
+package model;
+
+public class subProduct2 extends Product{
+
+	public subProduct2(String productName, String companyName, int quantitySoid, double price, double totalPrice) {
+		super(productName, companyName, quantitySoid, price, totalPrice);
+	}
+
+}
