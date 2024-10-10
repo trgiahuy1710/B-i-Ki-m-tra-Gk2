dbd8f9c6b1bcb6fe81dc205a4e84688c0be4d610
TrGiaHuy1710 <151889692+trgiahuy1710@users.noreply.github.com>
2024-10-11 00:43:30 +0700

    Delete thuchanh10_10 directory

--- thuchanh10_10/src/model/subProduct1.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class subProduct1 extends  Product{
-	
-	protected subProduct1(String productName, String companyName, int quantitySoid, double price, double totalPrice) {
-		super(productName, companyName, quantitySoid, price, totalPrice);
-	}
-	
-}
--- thuchanh10_10/src/model/subProduct2.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class subProduct2 extends Product{
-
-	public subProduct2(String productName, String companyName, int quantitySoid, double price, double totalPrice) {
-		super(productName, companyName, quantitySoid, price, totalPrice);
-	}
-
-}
